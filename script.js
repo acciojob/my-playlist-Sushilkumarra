f83dbbaefde0707b48e19c8157042fbdb23230af
Sushil Kumar Rajput <96855793+Sushilkumarra@users.noreply.github.com>
2025-10-18 11:53:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+// Simple JS functionality – shows an alert when media starts playing
+
+const videoPlayer = document.getElementById('videoPlayer');
+const audioPlayer = document.getElementById('audioPlayer');
+
+videoPlayer.addEventListener('play', () => {
+  console.log("🎬 Video is now playing!");
+});
+
+audioPlayer.addEventListener('play', () => {
+  console.log("🎵 Audio is now playing!");
+});
+
